50ee864bd65b48e6224199b33dfef035c2c9315b
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-05-13 04:19:24 +0200

    add some changes

--- app/config/mongo.js
+++ app/config/mongo.js
@@ -4,10 +4,14 @@ const mongoose = require('mongoose')
 
 const mongoCredentials = configs.db.mongo
 
+const uri = "mongodb+srv://hatem:webserver@cluster0.t0ute.mongodb.net/clubhouse?retryWrites=true&w=majority";
+
 const connect = async () => {
 
-    await mongoose.connect(mongoCredentials.connection_url, mongoCredentials.connection_options)
+    // await mongoose.connect(mongoCredentials.connection_url, mongoCredentials.connection_options)
+    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 }
+
 
 connect()
 
