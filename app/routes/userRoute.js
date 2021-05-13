df337c052afccf267b99d1efd9b17e04be90c3aa
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-05-13 02:28:05 +0200

    add some changes

--- app/routes/userRoute.js
+++ app/routes/userRoute.js
@@ -1,16 +1,20 @@
 const { getUserByPhoneNumber, createNewUser } = require('../controllers/userController')
 
-const { userValidator , allowedToAccess} = require('./../middlewares/validatorMiddleware')
+const { userValidator, allowedToAccess } = require('./../middlewares/validatorMiddleware')
 
-const {checkValidationError} = require('./../helpers/validatorError')
+const { checkValidationError } = require('./../helpers/validatorError')
 
 
 const express = require('express')
 
 const userRouter = express.Router()
 
+userRouter.get('/me', function (request, response) {
+    return response.send('Hello Yall Dardasha User Service');
+})
+
 userRouter.post('/info', allowedToAccess, getUserByPhoneNumber)
 
-userRouter.post('/create',allowedToAccess, userValidator(), checkValidationError, createNewUser)
+userRouter.post('/create', allowedToAccess, userValidator(), checkValidationError, createNewUser)
 
 module.exports = userRouter;
