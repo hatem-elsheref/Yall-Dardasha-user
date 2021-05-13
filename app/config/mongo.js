b0a7fbe50cc4be569c2e9cbad7115008ff1308d1
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-05-13 04:31:48 +0200

    add some changes

--- app/config/mongo.js
+++ app/config/mongo.js
@@ -9,7 +9,7 @@ const uri = "mongodb+srv://hatem:webserver@cluster0.t0ute.mongodb.net/clubhouse?
 const connect = async () => {
 
     // await mongoose.connect(mongoCredentials.connection_url, mongoCredentials.connection_options)
-    await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 }
 
 
