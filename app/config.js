c2010fb17e6a7cab4f8cb0f19d597601d86cd177
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-06-20 01:12:12 +0200

    deploy to herouk

--- app/config.js
+++ app/config.js
@@ -1,33 +1,33 @@
 module.exports = {
-    serviceName : 'user service',
-    serviceDescription : 'user service for create user , find user, update user, delete user',
-    apiKey : '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
-    jwt : {
-        secret : 'W7bwoFsqizrvyDUfQxdgSB2tN6JAmCYK',
+    serviceName: 'user service',
+    serviceDescription: 'user service for create user , find user, update user, delete user',
+    apiKey: '11bf5b37-e0b8-42e0-8dcf-dc8c4aefc000',
+    jwt: {
+        secret: 'W7bwoFsqizrvyDUfQxdgSB2tN6JAmCYK',
         options: {
             expiresIn: '24h',
             algorithm: 'HS256'
         }
     },
-    development : {
-        port : 3001,
-        url : 'http://localhost:3001'
+    development: {
+        port: 3001,
+        url: 'http://localhost:3001'
     },
-    production : {
-        port : process.env.PORT,
-        url : 'https://yalla-dardasha-user.herokuapp.com'
+    production: {
+        port: process.env.PORT,
+        url: 'https://yalla-dardasha-user.herokuapp.com'
     },
-    mongo : {
-        mongoDevelopmentUrl : 'mongodb://localhost/gp_clubhouse',
-        mongoProductionUrl : 'mongodb+srv://hatem:webserver@cluster0.t0ute.mongodb.net/clubhouse?retryWrites=true&w=majority',
+    mongo: {
+        mongoDevelopmentUrl: 'mongodb://localhost/gp_clubhouse',
+        mongoProductionUrl: 'mongodb+srv://hatem:webserver@cluster0.t0ute.mongodb.net/clubhouse?retryWrites=true&w=majority',
         // mongoProductionUrl : 'mongodb+srv://hatem:webserver@clubhouse.z7yno.mongodb.net/clubhouse?retryWrites=true&w=majority',
 
-        options : {
+        options: {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
             useCreateIndex: true
         }
     },
-    devEnvironment : true
+    devEnvironment: false
 }
